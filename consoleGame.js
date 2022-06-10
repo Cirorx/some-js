640edcd5f2fdb31dcf0df8e09d8733583f00cacd
Ciro <ciro.gronrroz@fing.edu.uy>
2022-06-10 13:13:03 -0300

    Big changes
    css was removed, only html and js was left
    
    js code has been cleared

--- consoleGame.js
+++ consoleGame.js
@@ -1,43 +1,43 @@
 const moves = ["rock", "paper", "scissors"];
 const results = ["T", "W", "L"]; //from USER perspective
-const r = "rock", p = "paper", s = "scissors";
 let winList = [];
 
 //Define winner
 function getStronger (move1, move2) {
-    var index;
-    if (move1 == r) {
-        if (move2 ==r) {
+    var index, result;    
+    //rock possibility
+    if (move1 == moves[0]) {
+        if (move2 == moves[0]) {
             index = 0;
         }//tide
-        else if (move2 == p) {
+        else if (move2 == moves[1]) {
             index = 2;
         }//loss
         else {index = 1}//won        
-    }//rock possibility
-
-    if (move1 == p) {
-        if (move2 ==r) {
+    }
+    //paper possibility
+    if (move1 == moves[1]) {
+        if (move2 == moves[0]) {
             index = 1;
         }
-        else if (move2 == p) {
+        else if (move2 == moves[1]) {
             index = 0;
         }
         else {index = 2}        
-    }//paper possibility
-
-    if (move1 == s) {
-        if (move2 ==r) {
+    }
+    //scissors possibility
+    if (move1 == moves[2]) {
+        if (move2 == moves[0]) {
             index = 2;
         }
-        else if (move2 == p) {
+        else if (move2 == moves[1]) {
             index = 1;
         }
         else {index = 0}        
-    }//scissors possibility
-
-    return index;
-    console.log("you are inside getStronger");
+    }
+    result = results[index];
+    
+    return result;    
 }
 
 //Make the computer play a random move
@@ -45,50 +45,41 @@ function computerPlay() {
     var move = "", random = null;
 
     //Gather info for Math random
-    min = Math.ceil(0);
-    max = Math.floor(moves.length);
-    random = Math.floor(Math.random() * (max - min + 1) + min);
-
-    console.log("you are inside Computerplay");
-    return moves[random];
+    random = Math.floor(Math.random() * 3) + 1;
+    move = moves[random];
+    return move;
 }
 
 function playRound(playerSelection) {
     computerSelection = computerPlay();
     outcome = getStronger(playerSelection, computerSelection);
-    return results[outcome];
 
-    console.log("you are inside playRound");
+    console.log("Computer has chosen " + computerSelection);
+    console.log("You chosed " + playerSelection);
+
+    console.log("////////////////////////////");
+    switch (outcome) {
+        case "T" : console.log("You have a tide with the computer, try again!");
+        case "W" : console.log("You have won  against the computer, good job! :)");
+        case "L" : console.log("You have lost the round, better luck next time! :D");
+    }    
 }//end playRound
 
 function game() {
     resultsList = [];
-    console.log("LETS PLAY");
-    console.log("/////////////////////");
+    console.log("                   /////////////////////");
+    console.log("                         LETS PLAY");
+    console.log("                   /////////////////////");
     
-    userMove = prompt("Write rock, paper or scissors to play").toLowerCase;
-
-    while (userMove == null) {
-        userMove = prompt("Write rock, paper or scissors to play").toLowerCase;
-    }
-
-    if (moves.includes(userMove)) {
-        result = playRound(userMove).toUpperCase;
-        switch (result) {
-            case "T" : console.log("You tide! :/")
-            case "W" : console.log("You won! :) ") 
-            case "L" : console.log("You lost! :( ")
-        }
-         results.concat(result);
-         console.log("This is your current history:")
-         console.log(results)
-     }
+    userMove = prompt("Write \"rock\", \"paper\" or \"scissors\" to play!");
+    /*} while (userMove !== "rock" /*|| userMove != "paper" || userMove != "scissors")*/
+   
+    playRound(userMove);
 }//end game
 
 // //let's play 5 rounds!
 // for (let i = 0; i < 5; i++) {
 //     game()
 // };
-
 
 game();
